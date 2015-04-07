960fdffb7cd93205f27776d8aa88660b9974c9fa
guille209 <gguillemiranda@gmail.com>
2015-04-07 18:47:36 +0200

    Practica terminada

--- src/points/ClosedPoint.java
+++ src/points/ClosedPoint.java
@@ -4,42 +4,35 @@ public class ClosedPoint extends Point {
 
 	public ClosedPoint(double value) {
 		super(value);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public boolean isGreaterOrEquals(ClosedPoint p) {
-		// TODO Auto-generated method stub
 		return greaterOrEquals(p);
 	}
 
 	@Override
 	public boolean isGreaterOrEquals(OpenedPoint p) {
-		// TODO Auto-generated method stub
 		return this.value > p.value;
 	}
 
 	@Override
 	public boolean isLessOrEquals(ClosedPoint p) {
-		// TODO Auto-generated method stub
 		return lessOrEquals(p);
 	}
 
 	@Override
 	public boolean isLessOrEquals(OpenedPoint p) {
-		// TODO Auto-generated method stub
 		return this.value < p.value;
 	}
 
 	@Override
 	public boolean checkMinimum(Point p) {
-		// TODO Auto-generated method stub
 		return p.isGreaterOrEquals(this);
 	}
 
 	@Override
 	public boolean checkMaximum(Point p) {
-		// TODO Auto-generated method stub
 		return p.isLessOrEquals(this);
 	}
 }
--- src/points/Point.java
+++ src/points/Point.java
@@ -10,11 +10,11 @@ public abstract class Point {
 	public double getValue() {
 		return value;
 	}
-	
+
 	public double midPoint(Point point) {
 		return (this.getValue() + point.getValue()) / 2;
 	}
-	
+
 	public boolean greaterOrEquals(Point p) {
 		return value >= p.value;
 	}
@@ -22,15 +22,17 @@ public abstract class Point {
 	public boolean lessOrEquals(Point p) {
 		return value <= p.value;
 	}
-	
+
 	public abstract boolean checkMinimum(Point p);
-	
+
 	public abstract boolean checkMaximum(Point p);
-	
+
 	public abstract boolean isGreaterOrEquals(ClosedPoint p);
+
 	public abstract boolean isGreaterOrEquals(OpenedPoint p);
-	
+
 	public abstract boolean isLessOrEquals(ClosedPoint p);
+
 	public abstract boolean isLessOrEquals(OpenedPoint p);
 
 }
